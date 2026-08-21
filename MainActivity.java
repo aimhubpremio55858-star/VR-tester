b1569ea672ec289bcaf1f5728197fcdb80ebfed5
aimhubpremio55858-star <aim.hub.premio55858@gmail.com>
2026-08-21 17:34:16 -0300

    Refactor MainActivity for AppCompat and WebView setup
    Refactor MainActivity to extend AppCompatActivity and streamline WebView setup. Remove unnecessary camera permission checks and consolidate WebView settings.

--- MainActivity.java
+++ MainActivity.java
@@ -1,92 +1,42 @@
-package com.vrbox.mr;
+package com.vr.tester;
 
-import android.Manifest;
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.content.pm.PackageManager;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
 
-    private static final int CAMERA_PERMISSION = 100;
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-        // Permissão da câmera
-        if (
-            checkSelfPermission(
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-
-            requestPermissions(
-                new String[]{
-                    Manifest.permission.CAMERA
-                },
-                CAMERA_PERMISSION
-            );
-        }
-
-
-        // WebView
-        webView =
-            new WebView(this);
-
-
-        WebSettings settings =
-            webView.getSettings();
-
-
-        settings.setJavaScriptEnabled(true);
-
-        settings.setDomStorageEnabled(true);
-
-        settings.setMediaPlaybackRequiresUserGesture(
-            false
-        );
-
-
-        webView.setWebViewClient(
-            new WebViewClient()
-        );
-
-
-        webView.setWebChromeClient(
-            new WebChromeClient() {
-
-                @Override
-                public void onPermissionRequest(
-                    PermissionRequest request
-                ) {
-
-                    runOnUiThread(() -> {
-
-                        request.grant(
-                            request.getResources()
-                        );
-
-                    });
-
-                }
-
-            }
-        );
-
-
-        webView.loadUrl(
-            "file:///android_asset/index.html"
-        );
-
-
+        
+        webView = new WebView(this);
         setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Concede permissões de câmera e WebXR requisitadas pela página web
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+
+        // Carrega o arquivo index.html local ou o link do GitHub Pages
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
